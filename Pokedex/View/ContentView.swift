d2c75c451dd0504c84fb7c456b48a421e0884b1b
Luis Calvillo <luiscalvillo89@gmail.com>
2024-03-20 00:09:23 -0700

    Added Pokemon to ContentView

--- Pokedex/View/ContentView.swift
+++ Pokedex/View/ContentView.swift
@@ -11,10 +11,25 @@ struct ContentView: View {
     
     @StateObject var vm = ViewModel()
     
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
         NavigationView {
-           
-            Text("Hello, world!")
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 10){
+                    ForEach(vm.filteredPokemon) { pokemon in
+                        NavigationLink(destination: PokemonDetailView(pokemon: pokemon)) {
+                            PokemonView(pokemon: pokemon)
+                        }
+                    }
+                }
+                .animation(.easeIn(duration: 0.3), value: vm.filteredPokemon.count)
+                .navigationTitle("Pokedex")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .searchable(text: $vm.searchText)
         }
         .environmentObject(vm)
     }
