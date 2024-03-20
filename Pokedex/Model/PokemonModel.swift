fe958b8882f6b37c5a481581092c5ada483d48ed
Luis Calvillo <luiscalvillo89@gmail.com>
2024-03-19 22:01:47 -0700

    Added PokemonModel

---
+++ Pokedex/Model/PokemonModel.swift
@@ -0,0 +1,28 @@
+//
+//  PokemonModel.swift
+//  Pokedex
+//
+//  Created by Luis Calvillo on 3/19/24.
+//
+
+import Foundation
+
+struct PokemonPage: Codable {
+    let count: Int
+    let next: String
+    let results: [Pokemon]
+}
+
+struct Pokemon: Codable, Identifiable, Equatable {
+    let id = UUID()
+    let name: String
+    let url: String
+    
+    static var samplePokemon = Pokemon(name: "Bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/")
+}
+
+struct DetailPokemon: Codable {
+    let id: Int
+    let height: Int
+    let weight: Int
+}
